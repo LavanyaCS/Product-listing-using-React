a869823b497a685419e111c21aa1d994d81bd71f
LavanyaCS <lavanyachandranece@gmail.com>
2025-06-17 12:58:30 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   base: '/Product-listing-using-React/', // ✅ this must match your repo name
+  base: '/Product-listing-using-React/', // ✅ this must match your repo name
+  plugins: [react(), tailwindcss()],
 })
