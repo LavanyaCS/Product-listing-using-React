69ddb2b61c27da9f10fc894a8680c27069c58beb
LavanyaCS <lavanyachandranece@gmail.com>
2025-06-17 14:42:18 +0530

    Updates

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-     <div className="p-6 text-center text-xl">We sell awesome products!</div>
-  )
-}
-
-export default About
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Product-listing-using-React/', // ✅ this must match your repo name
-  plugins: [react(), tailwindcss()],
-})
